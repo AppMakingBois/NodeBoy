ab33b2c72a2147c2a1b50abe84c398d18a981388
Evan <evan.straw99@gmail.com>
2018-01-24 23:32:15 -0800

    Add test to make sure that encoding works properly
    The DataTypeEncodeTests class contains JUnit tests that test whether output
    from encoding and then decoding data is the same as the original data.
    If these tests fail, I know something is wrong with my encode/decode
    programming.

---
+++ app/src/test/java/io/github/appmakingbois/nodeboy/DataTypeEncodeTests.java
@@ -0,0 +1,63 @@
+package io.github.appmakingbois.nodeboy;
+
+
+import org.junit.Test;
+
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+import io.github.appmakingbois.nodeboy.protocol.PacketDecoder;
+import io.github.appmakingbois.nodeboy.protocol.PacketEncoder;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class DataTypeEncodeTests {
+    @Test
+    public void encodeUUIDTest(){
+        System.out.println("Testing if UUID encoding works...");
+        UUID testUUID = UUID.randomUUID();
+        assertTrue(PacketDecoder.decodeUUID(PacketEncoder.encodeUUID(testUUID)).compareTo(testUUID) == 0);
+    }
+
+    @Test
+    public void encodeIntTest(){
+        System.out.println("Testing if VarInt encoding works...");
+        int testNum = 420;
+        assertTrue(PacketDecoder.decodeVarInt(PacketEncoder.encodeVarInt(testNum)) == testNum);
+    }
+
+    @Test
+    public void encodeNegativeIntTest(){
+        System.out.println("Testing if VarInt encoding works with negative numbers...");
+        int testNum = -1337;
+        assertTrue(PacketDecoder.decodeVarInt(PacketEncoder.encodeVarInt(testNum)) == testNum);
+    }
+
+    @Test
+    public void encodeLongTest(){
+        System.out.println("Testing if VarLong encoding works...");
+        long testNum = 1234567890;
+        assertTrue(PacketDecoder.decodeVarLong(PacketEncoder.encodeVarLong(testNum)) == testNum);
+    }
+
+    @Test
+    public void encodeBooleanTest(){
+        System.out.println("Testing if Boolean encoding works...");
+        boolean t = true;
+        boolean f = false;
+        assertTrue(PacketDecoder.decodeBoolean(PacketEncoder.encodeBoolean(t)));
+        assertFalse(PacketDecoder.decodeBoolean(PacketEncoder.encodeBoolean(f)));
+    }
+
+    @Test
+    public void arrayConversionTest(){
+        System.out.println("Testing if converting between byte arrays and ArrayList<Byte> works...");
+        byte[] bytes = new byte[3];
+        bytes[0] = 0x10;
+        bytes[1] = 0x42;
+        bytes[2] = 0x69;
+        assertArrayEquals(bytes, PacketDecoder.byteListToArray(PacketEncoder.byteArrayToList(bytes)));
+    }
+}
