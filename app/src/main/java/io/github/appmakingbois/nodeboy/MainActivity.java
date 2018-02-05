95fecb521ed1751dba0e1b6ff1f03d824579fd85
ev4nx99 <evan.straw99@gmail.com>
2018-02-05 10:26:53 -0800

    Add a ChatActivity where the main function of the application will take place

--- app/src/main/java/io/github/appmakingbois/nodeboy/MainActivity.java
+++ app/src/main/java/io/github/appmakingbois/nodeboy/MainActivity.java
@@ -1,7 +1,10 @@
 package io.github.appmakingbois.nodeboy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +14,13 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), ChatActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
