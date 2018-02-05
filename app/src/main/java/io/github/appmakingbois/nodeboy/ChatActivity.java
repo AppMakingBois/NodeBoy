9343273799bebb030d3e2756baec24197a8d9e33
ev4nx99 <evan.straw99@gmail.com>
2018-02-05 11:11:22 -0800

    Create chat box UI elements and some code to insert message (WARNING: UNTESTED)

--- app/src/main/java/io/github/appmakingbois/nodeboy/ChatActivity.java
+++ app/src/main/java/io/github/appmakingbois/nodeboy/ChatActivity.java
@@ -2,6 +2,12 @@ package io.github.appmakingbois.nodeboy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO make actual chat box functionality
+            }
+        });
+    }
+
+    private void insertOutgoingMessage(String senderID, String messageBody){
+        LayoutInflater inflater = getLayoutInflater();
+        LinearLayout messageView = (LinearLayout) inflater.inflate(R.layout.message_outgoing,null);
+        TextView senderNameDisplay = messageView.findViewById(R.id.senderName);
+        senderNameDisplay.setText(senderID);
+        TextView messageBodyDisplay = messageView.findViewById(R.id.messageBody);
+        messageBodyDisplay.setText(messageBody);
+        TextView recievedTimeDisplay = messageView.findViewById(R.id.receivedTime);
+        Date time = new Date();
+        recievedTimeDisplay.setText(time.getHours()+":"+time.getMinutes());
+        LinearLayout container = findViewById(R.id.chatContainer);
+        container.addView(messageView);
     }
 }
