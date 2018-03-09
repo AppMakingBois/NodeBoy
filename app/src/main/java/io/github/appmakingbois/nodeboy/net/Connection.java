ff661dc9a02f4a6bad8dafc33caf519155aa24b9
Evan <evan.straw99@gmail.com>
2018-03-09 14:07:17 -0800

    Change connection management system
    ConnectionManager is now completely concurrent, couldn't prevent ConcurrentModificationExceptions with the old method
    Connection is also used instead of WifiP2pDevice, it holds a little more information as well as any currently open socket communicating with the device.

--- app/src/main/java/io/github/appmakingbois/nodeboy/net/Connection.java
+++ app/src/main/java/io/github/appmakingbois/nodeboy/net/Connection.java
@@ -1,11 +1,35 @@
 package io.github.appmakingbois.nodeboy.net;
 
 import android.net.wifi.p2p.WifiP2pDevice;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.net.Socket;
 
 public class Connection {
-    private WifiP2pDevice deviceInfo;
+    private WifiP2pDevice device;
     private Socket socket;
-    private int state;
+    private long lastKeepAlive;
+
+    public Connection(@NonNull WifiP2pDevice device){
+        this.device = device;
+    }
+
+    @Nullable
+    public Socket getSocket(){
+        return this.socket;
+    }
+
+    public boolean isSocketOpen() {
+        return this.socket != null && !this.socket.isClosed();
+    }
+
+    @NonNull
+    public WifiP2pDevice getDevice(){
+        return this.device;
+    }
+
+    public void setSocket(@NonNull Socket socket){
+        this.socket = socket;
+    }
 }
--- app/src/main/java/io/github/appmakingbois/nodeboy/net/ConnectionManager.java
+++ app/src/main/java/io/github/appmakingbois/nodeboy/net/ConnectionManager.java
@@ -6,81 +6,51 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
-    private ArrayList<WifiP2pDevice> connections;
+    private ConcurrentHashMap<String,Connection> connectionMap;
 
-    public synchronized ArrayList<WifiP2pDevice> getConnections() {
-        return copyList(connections);
+    public ArrayList<Connection> getConnections() {
+        return mapToArray(connectionMap);
     }
 
     @Nullable
-    public synchronized WifiP2pDevice getDeviceByIndex(int i) {
-        try {
-            return copyDevice(connections.get(i));
-        } catch (ArrayIndexOutOfBoundsException e) {
-            return null;
-        }
+    public Connection getDeviceByAddress(@NonNull String address) {
+        return connectionMap.get(address);
     }
 
-    @Nullable
-    public synchronized WifiP2pDevice getDeviceByAddress(@NonNull String address) {
-        for (WifiP2pDevice d : connections) {
-            if (d.deviceAddress.equalsIgnoreCase(address)) {
-                return copyDevice(d);
-            }
-        }
-        return null;
-    }
-
-    public synchronized void addConnection(@NonNull WifiP2pDevice device) {
+    public void addConnection(@NonNull WifiP2pDevice device) {
         if(getDeviceByAddress(device.deviceAddress)==null){
-            connections.add(copyDevice(device));
+            connectionMap.put(device.deviceAddress,new Connection(device));
         }
     }
 
-    public synchronized boolean removeConnection(int index) {
-        WifiP2pDevice result = getDeviceByIndex(index);
-        if (result != null) {
-            connections.remove(index);
-            return true;
-        }
-        else {
-            return false;
-        }
+    public boolean removeConnection(@NonNull String address) {
+        Connection result = getDeviceByAddress(address);
+        return result != null && connectionMap.remove(address) != null;
     }
 
-    public synchronized boolean removeConnection(@NonNull String address) {
-        WifiP2pDevice result = getDeviceByAddress(address);
-        if(result!=null){
-            connections.remove(result);
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
-    public synchronized boolean removeConnection(@NonNull WifiP2pDevice device){
+    public boolean removeConnection(@NonNull WifiP2pDevice device){
         return removeConnection(device.deviceAddress);
     }
 
-    public synchronized void removeAllConnections(){
-        synchronized (this) {
-            for (WifiP2pDevice device : connections) {
-                removeConnection(device);
-            }
+    public void removeAllConnections(){
+        Iterator i = connectionMap.entrySet().iterator();
+        while(i.hasNext()){
+            removeConnection(((Connection) i.next()).getDevice());
+            i.remove();
         }
     }
 
     @NonNull
-    private synchronized WifiP2pDevice copyDevice(@NonNull WifiP2pDevice device) {
+    private WifiP2pDevice copyDevice(@NonNull WifiP2pDevice device) {
         return new WifiP2pDevice(device);
     }
 
     @NonNull
-    private synchronized ArrayList<WifiP2pDevice> copyList(@NonNull ArrayList<WifiP2pDevice> input) {
+    private ArrayList<WifiP2pDevice> copyList(@NonNull ArrayList<WifiP2pDevice> input) {
         ArrayList<WifiP2pDevice> output = new ArrayList<>();
         for (WifiP2pDevice d : input) {
             output.add(new WifiP2pDevice(d));
@@ -88,10 +58,21 @@ public class ConnectionManager {
         return output;
     }
 
+    @NonNull
+    private ArrayList<Connection> mapToArray(@NonNull ConcurrentHashMap<String,Connection> input){
+        Iterator i = input.entrySet().iterator();
+        ArrayList<Connection> output = new ArrayList<>();
+        while(i.hasNext()){
+            output.add((Connection) i.next());
+            i.remove();
+        }
+        return output;
+    }
+
     private static ConnectionManager instance;
 
     private ConnectionManager() {
-        connections = new ArrayList<>();
+        connectionMap = new ConcurrentHashMap<>();
     }
 
     @NonNull
