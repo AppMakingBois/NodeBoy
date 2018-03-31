1fa0eeb02d2a7f371e1c2a64d8567c1a286f0f44
Evan <evan.straw99@gmail.com>
2018-03-31 01:08:24 -0700

    Move activities to their own package

--- app/src/main/java/io/github/appmakingbois/nodeboy/activity/MainActivity.java
+++ app/src/main/java/io/github/appmakingbois/nodeboy/activity/MainActivity.java
@@ -1,9 +1,13 @@
-package io.github.appmakingbois.nodeboy;
+package io.github.appmakingbois.nodeboy.activity;
 
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+
+import io.github.appmakingbois.nodeboy.R;
+import io.github.appmakingbois.nodeboy.fragment.P2PCheckFragment;
+import io.github.appmakingbois.nodeboy.fragment.P2PFailFragment;
 
 public class MainActivity extends AppCompatActivity implements P2PFailFragment.OnFragmentInteractionListener,P2PCheckFragment.OnFragmentInteractionListener {
 
